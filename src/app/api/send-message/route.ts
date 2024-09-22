edd66038e9fad4b84f29ff75b184e9e61352ef2d
Amisha <amisha9565@gmail.com>
2024-09-22 22:58:06 +0530

    send message post api

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,68 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+export async function POST(request: Request) {
+    
+    await dbConnect();
+
+    const { username, content } = await request.json();
+
+    try {
+
+        const user = await UserModel.findOne({ username })
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found!"
+                },
+                { status: 404 }
+            )
+        }
+
+        // is user accepting the messages
+
+        if (user.isAcceptingMessage) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User is not accepting messages"
+                },
+                {
+                    status: 403 //forbidden status
+                }
+            )
+        }
+
+        const newMessage = { content, createdAt: new Date() }
+
+        user.messages.push(newMessage as Message);
+
+        await user.save();
+
+        return Response.json(
+            {
+                success: true,
+                message: "Message sent successfully!"
+            },
+            {
+                status: 403 //forbidden status
+            }
+        )
+
+    } catch (error) {
+
+        console.log('error adding messages ', error);
+
+        return Response.json(
+            {
+                success: false,
+                message: "Internal server error"
+            },
+            {
+                status: 500 //forbidden status
+            }
+        )
+    }
+}
